d52ba981d46f8d81d9369dea5411e2473a0589d7
Sam-GTECH <115488661+Sam-GTECH@users.noreply.github.com>
2024-10-14 17:17:05 +0200

    Very shaky balls implementation
    May sound wrong out of context

--- src/Projet2-Encapsule/main.cpp
+++ src/Projet2-Encapsule/main.cpp
@@ -1,6 +1,59 @@
 #include "pch.h"
 #include "main.h"
 
+class Ball_temp
+{
+public:
+	Ball_temp(float lifetime)
+	{
+		this->lifetime = lifetime;
+
+		x = 150;
+		y = 150;
+
+		ball.setRadius(lifetime);
+
+		float rad = ball.getRadius();
+		ball.setOrigin(rad/2, rad/2);
+
+		speed = 0.1;
+	}
+
+	sf::CircleShape ball;
+
+	float lifetime;
+
+	float x;
+	float y;
+
+	int dirX = 1;
+	int dirY = 1;
+
+	float speed = 2;
+
+	void draw(sf::RenderWindow* window)
+	{
+		lifetime -= 0.001;
+
+		sf::Vector2u size = window->getSize();
+		float rad = ball.getRadius();
+
+		int futureX = x + dirX * speed;
+		int futureY = y + dirY * speed;
+
+		if (futureX > size.x-rad || futureX < 0)
+			dirX = -dirX;
+		if (futureY > size.y-rad || futureY < 0)
+			dirY = -dirY;
+
+		x += dirX * speed;
+		y += dirY * speed;
+
+		ball.setPosition(x, y);
+		window->draw(ball);
+	}
+};
+
 LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam);
 
 int APIENTRY wWinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE hPrevInstance, _In_ LPWSTR lpCmdLine, _In_ int nCmdShow)
@@ -28,14 +81,57 @@ int APIENTRY wWinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE hPrevInstance
 	if ( hWnd==NULL )
 		return 0;
 
+#ifdef _DEBUG
+	AllocConsole();
+	FILE* pNewStdout = nullptr;
+	FILE* pNewStderr = nullptr;
+	FILE* pNewStdin = nullptr;
+
+	::freopen_s(&pNewStdout, "CONOUT$", "w", stdout);
+	::freopen_s(&pNewStderr, "CONOUT$", "w", stderr);
+	::freopen_s(&pNewStdin, "CONIN$", "r", stdin);
+#endif
+
+	sf::RenderWindow window(hWnd);
+
 	ShowWindow(hWnd, nCmdShow);
 	UpdateWindow(hWnd);
 
+	float timer = 0;
+	std::vector<Ball_temp*> balls;
+
 	MSG msg;
-	while ( GetMessage(&msg, nullptr, 0, 0) )
+	while (window.isOpen())
 	{
-		TranslateMessage(&msg);
-		DispatchMessage(&msg);
+		std::cout << timer << std::endl;
+		timer+=0.333; // A adapter au delta time!
+		while (PeekMessage(&msg, nullptr, 0, 0, PM_REMOVE))
+		{
+			TranslateMessage(&msg);
+			DispatchMessage(&msg);
+		}
+
+		window.clear();
+
+		if (timer > 600)
+		{
+			timer = 0;
+			Ball_temp* ball = new Ball_temp(60.f);
+			balls.push_back(ball);
+		}
+
+		for (size_t i =balls.size(); i > 0; i--)
+		{
+			Ball_temp* b = balls[i-1];
+			b->draw(&window);
+			if (balls[i-1]->lifetime <= 0.f)
+			{
+				delete balls[i-1];
+				balls.erase(balls.begin() + (i-1));
+			}
+		}
+
+		window.display();
 	}
 
 #ifdef _DEBUG
--- src/Projet2-Encapsule/pch.h
+++ src/Projet2-Encapsule/pch.h
@@ -2,9 +2,22 @@
 
 #include <SFML/Graphics.hpp>
 
+#include <iostream>
+#include <vector>
 #include <windows.h>
 
 #include "resource.h"
+
+#include "Maths.h"
+
+#include "Texture.h"
+#include "Sprite.h"
+#include "Window.h"
+#include "Renderer.h"
+#include "Ball.h"
+
+#include "Gameplay.h"
+#include "PerformanceTester.h"
 
 #ifdef _DEBUG
 #include <crtdbg.h>
